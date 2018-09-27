146bbb3bdd162761f0c14466017a36dadb5babfe
trippyt <40070411+trippyt@users.noreply.github.com>
2018-09-27 18:53:26 +0100

    Add files via upload

---
+++ dummyGPIO.py
@@ -0,0 +1,10 @@
+OUT = 'out'
+BCM = 'bcm'
+def setwarnings(*args, **kwargs):
+    print('setwarnings args {} kwargs {}'.format(args, kwargs))
+def setup(*args, **kwargs):
+    print('setup args {} kwargs {}'.format(args, kwargs))
+def output(*args, **kwargs):
+    print('output args {} kwargs {}'.format(args, kwargs))
+def setmode(*args, **kwargs):
+    print('setmode args {} kwargs {}'.format(args, kwargs))
